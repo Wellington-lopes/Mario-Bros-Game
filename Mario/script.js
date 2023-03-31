88a04ad46300ba53ca912def89b755f15db7f8ec
Wellington Lopes <wellington.slopes1991@gmail.com>
2023-03-31 17:29:07 -0300

    Finalizando o joguinho

--- Mario/script.js
+++ Mario/script.js
@@ -1,4 +1,5 @@
 const mario = document.querySelector('.mario');
+const cano = document.querySelector('.cano');
 
 const jump = () => {
     mario.classList.add('jump');
@@ -9,5 +10,26 @@ const jump = () => {
 
     },500);
 }
+
+const loop = setInterval(() => {
+
+    const canoPosition = cano.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+  
+    if (canoPosition <= 105 && canoPosition > 0 && marioPosition < 85) {
+        
+        cano.style.animation = 'none';
+        cano.style.left = `${canoPosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = './gif mario/morte.webp'
+
+        clearInterval(loop);
+    }
+    
+
+}, 10)
 
 document.addEventListener('keydown', jump);
